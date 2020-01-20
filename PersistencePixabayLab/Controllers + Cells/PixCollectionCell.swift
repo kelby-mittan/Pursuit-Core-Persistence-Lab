26dcca17b4cb2953ff8a233616f4c30f4a47b386
Kelby Mittan <kelbymittan@pursuit.org>
2020-01-20 16:39:19 -0500

    setting up pix controller

--- PersistencePixabayLab/Controllers + Cells/PixCollectionCell.swift
+++ PersistencePixabayLab/Controllers + Cells/PixCollectionCell.swift
@@ -7,11 +7,26 @@
 //
 
 import UIKit
+import ImageKit
 
 class PixCollectionCell: UICollectionViewCell {
     
     @IBOutlet var pixabayImage: UIImageView!
     
-    
+    func configureCell(for pix: PixImage) {
+        pixabayImage.getImage(with: pix.previewURL) { [weak self] (result) in
+            switch result {
+            case .failure:
+                DispatchQueue.main.async {
+                    self?.pixabayImage.image = UIImage(systemName: "photo.fill")
+                }
+            case .success(let image):
+                DispatchQueue.main.async {
+                    self?.pixabayImage.image = image
+                }
+            }
+        }
+        
+    }
     
 }
--- PersistencePixabayLab/Controllers + Cells/PixSearchController.swift
+++ PersistencePixabayLab/Controllers + Cells/PixSearchController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class PixSearchController: UIViewController {
 
+    
+    @IBOutlet var collectionView: UICollectionView!
+    @IBOutlet var searchBar: UISearchBar!
+    
+    var pixPics = [PixImage]() {
+        didSet {
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
+        }
+    }
+    
+    var searchQuery = "" {
+        didSet {
+            DispatchQueue.main.async {
+                self.loadPix(for: self.searchQuery.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!)
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    private func loadPix(for search: String) {
+        
+        PixAPIClient.getPix(for: search) { [weak self] (result) in
+            switch result {
+            case .failure(let appError):
+                DispatchQueue.main.async {
+                    self?.showAlert(title: "Error", message: "\(appError)")
+                }
+            case .success(let pics):
+                DispatchQueue.main.async {
+                    self?.pixPics = pics
+                }
+            }
+        }
+        
+    }
 
 }
 
