873526d2b225516150c248916f91428f78e1193b
Kelby Mittan <kelbymittan@pursuit.org>
2020-01-20 16:59:30 -0500

    able to get collectionView populating

--- PersistencePixabayLab/Controllers + Cells/PixSearchController.swift
+++ PersistencePixabayLab/Controllers + Cells/PixSearchController.swift
@@ -32,7 +32,11 @@ class PixSearchController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        searchBar.delegate = self
+        loadPix(for: searchQuery)
     }
 
     private func loadPix(for search: String) {
@@ -52,5 +56,67 @@ class PixSearchController: UIViewController {
         
     }
 
+}
+
+extension PixSearchController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pixPics.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "pixCell", for: indexPath) as? PixCollectionCell else {
+            fatalError("could not downcast country")
+        }
+        let pixPic = pixPics[indexPath.row]
+        cell.configureCell(for: pixPic)
+        
+        return cell
+    }
+}
+
+extension PixSearchController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let interItemSpacing: CGFloat = 4   // space between items
+        let maxWidth = UIScreen.main.bounds.size.width  // device's width
+        let numberOfItems: CGFloat = 2
+        let totalSpacing: CGFloat = numberOfItems * interItemSpacing
+        let itemWidth: CGFloat = (maxWidth - totalSpacing) / numberOfItems
+        
+//        return CGSize(width: itemWidth, height: (itemWidth * 2))
+        return CGSize(width: itemWidth, height: 300)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 5, left: 0, bottom: 5, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 4
+    }
+}
+
+
+extension PixSearchController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        searchBar.resignFirstResponder()
+        
+        guard let searchText = searchBar.text else {
+            return
+        }
+        searchQuery = searchText
+        
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        guard !searchText.isEmpty else {
+            loadPix(for: searchQuery)
+            return
+        }
+        
+        searchQuery = searchText
+    }
 }
 
